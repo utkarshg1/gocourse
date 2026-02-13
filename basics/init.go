e7647060bf8ef5fd235593f896c3535b58b1d5af
utkarshg1 <gaikwadujg@gmail.com>
2026-02-13 21:40:07 +0530

    init

---
+++ basics/init.go
@@ -0,0 +1,19 @@
+package basics
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initializing package 1 ...")
+}
+
+func init() {
+	fmt.Println("Initializing package 2 ...")
+}
+
+func init() {
+	fmt.Println("Initializing package 3 ...")
+}
+
+func main() {
+	fmt.Println("Main function")
+}
